03ef2651083d6440268ce9ed7e2f475546605261
Juan Salazar <juan.salazar@grability.com>
2017-03-21 08:58:25 -0500

    Se agrega express y se inicia el servidor node

---
+++ api-restful-favoritos/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+
+app.listen(3678, function() {
+    console.log('Api Restful favoritos arrancado en http://localhost:3678');
+});
