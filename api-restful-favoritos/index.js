52e0f2fb93756616b93d2f7e60470cc4dba2ea94
Juan Salazar <juan.salazar@grability.com>
2017-03-21 11:12:04 -0500

    Se Agrega nodemon y BodyParser

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -1,8 +1,14 @@
 'use strict'
 
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 
-app.listen(3678, function() {
-    console.log('Api Restful favoritos arrancado en http://localhost:3678');
+var app = express();
+var port = process.env.PORT || 3678;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(port, function() {
+    console.log(`Api Restful favoritos arrancado en http://localhost:${port}`);
 });
