3229fda7a7fcbbf24734dbb4250cf8fa3b480665
Juan Salazar <juan.salazar@grability.com>
2017-03-22 20:02:46 -0500

    Rutas y parametros

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -9,6 +9,16 @@ var port = process.env.PORT || 3678;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.get('/prueba:name', function(req, res) {
+
+    var name = req.params.name;
+
+    res.status(200).send({
+        data: [2,2,4],
+        texto: 'Hola mundo con NodeJS'
+    });
+});
+
 app.listen(port, function() {
     console.log(`Api Restful favoritos arrancado en http://localhost:${port}`);
 });
