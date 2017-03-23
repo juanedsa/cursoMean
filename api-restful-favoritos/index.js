03a7f0b8be8dc9bb3def39eeb0ab6a222ff63abd
Juan Salazar <juan.salazar@grability.com>
2017-03-23 15:28:18 -0500

    se agrega mongoose

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -1,8 +1,20 @@
 'use strict'
 
+var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3678;
 
-app.listen(port, function() {
-    console.log(`Api Restful favoritos arrancado en http://localhost:${port}`);
+mongoose.connect('mongodb://localhost:27017/cursofavoritos', (err, res) => {
+
+    if (err) {
+        throw err;
+    } else {
+        app.listen(port, function () {
+            console.log(`Api Restful favoritos arrancado en http://localhost:${port}`);
+        });
+
+        console.log('Conexion a Mongo DB exitosa');
+    }
+
+
 });
