5f1534e8c5d24a4fd7a0c773885f0ffeb452983a
Juan Salazar <juan.salazar@grability.com>
2017-03-23 20:55:27 -0500

    Listar y ordernar

--- api-restful-favoritos/controllers/favorito.js
+++ api-restful-favoritos/controllers/favorito.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Favorito = require('../models/favorito')
+
 function prueba(req, res) {
     var name = req.params.name;
 
@@ -19,14 +21,47 @@ function getFavorito(req, res) {
 
 function getFavoritos(req, res) {
 
+    Favorito.find({}).sort('-_id').exec((err, favoritos) => {
+        if (err) {
+            res.status(500).send({
+                message: 'Error al consultar todos los favoritos'
+            });
+        }
+
+        if (!favoritos) {
+            res.status(404).send({
+                message: 'No hay marcadores'
+            });
+        }
+
+        res.status(200).send({
+            favoritos: favoritos
+        });
+    });
+
 }
 
 function saveFavorito(req, res) {
+    var favorito = new Favorito();
     var params = req.body;
 
-    res.status(200).send({
-        favorito: params
+    favorito.title = params.title;
+    favorito.description = params.description;
+    favorito.url = params.url;
+
+    favorito.save((err, favoritoStored) => {
+        if (err) {
+            res.status(500).send({
+                message: 'Error al guardar el favorito'
+            });
+        }
+
+        res.status(200).send({
+            favorito: favoritoStored
+        });
+
     });
+
 }
 
 function updateFavorito(req, res) {
@@ -40,7 +75,7 @@ function updateFavorito(req, res) {
 
 function deleteFavorito(req, res) {
     var favoritoId = req.params.id;
-    
+
     res.status(200).send({
         data: favoritoId
     });
