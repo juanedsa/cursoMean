bba4d12e4bd07cb8c065549da20f2deb9301f184
Juan Salazar <juan.salazar@grability.com>
2017-03-30 20:57:22 -0500

    Se consume el listado de favoritos

--- front-favoritos/src/app/components/favoritos-list.component.ts
+++ front-favoritos/src/app/components/favoritos-list.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FavoritoService } from '../services/favorito.service';
+import { Favorito } from '../models/favorito';
 
 @Component({
   selector: 'app-favoritos-list',
-  templateUrl: '../views/favoritos-list.html'
+  templateUrl: '../views/favoritos-list.html',
+  providers: [FavoritoService]
 })
-export class FavoritosListComponent {
+export class FavoritosListComponent implements OnInit{
   public title: string;
+  public errorMessage: string;
+  public favoritos: Favorito[];
 
-  constructor() {
+  constructor(private favoritoService: FavoritoService) {
     this.title = 'Listado de Marcadores';
+  }
+
+  ngOnInit() {
+    console.log('Favoritos list Component cargado ');
+    this.favoritoService.getFavoritos().subscribe(
+      result => {
+        console.log(result);
+        this.favoritos = result.favoritos;
+
+        if(!this.favoritos) {
+          alert('Error en el servidor');
+        }
+      },
+      error => {
+        this.errorMessage = <any>error;
+
+        if(this.errorMessage !=null) {
+          console.log(this.errorMessage);
+          alert('Error en la peticion');
+        }
+      }
+    );
   }
 }
